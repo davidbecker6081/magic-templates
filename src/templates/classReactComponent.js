7dfffae9bbd901c8106af28b4fdfd9353d9b9902
David Becker <david@beckersite.com>
2020-01-08 17:19:14 -0700

    Add ability to create both a functional and class based component

---
+++ src/templates/classReactComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MyComponent extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>A Class/Stateful Compoonent</div>
+        )
+    }
+}
+
+export default MyComponent
